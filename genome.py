ffa9deb7f99c9a00a22e735908054700ee9927af
adam <risnergadam@gmail.com>
2016-05-20 11:31:55 +0000

    heatmap implemented

--- genome.py
+++ genome.py
@@ -1,17 +1,21 @@
-import pysam, sys
+import pysam, sys, pickledb
 from pyspark import SparkContext
+from operator import add
 
 # produces all k-mers from sequence given k-mer length k
+# returns a tuple on the form (id, [kmers...])
 def applyKMers(sequence, k):
     kMers = []
-    seq = sequence[0]
-    pos = sequence[1]
+    id = sequence[0]
+    seq = sequence[1]
+    pos = sequence[2]
     for i in range(0, len(seq) - k):
         kMers.append((seq[i: i+k], pos + i))
-    return kMers
+    return (id, kMers)
 
 # given a filename to a bam file, pysam is used to read and extract all sequences and their positions
 def extractSequences(fileName):
+    id = fileName[0:7]
     sequences = []
     url = swiftUrl + fileName
     with pysam.AlignmentFile(url, "rb") as samfile:
@@ -19,7 +23,7 @@ def extractSequences(fileName):
         for r in samfile.fetch(until_eof = True):
             if not r.is_unmapped: continue
             if len(sequences) > 100: break
-            sequences.append((r.query_sequence, r.reference_start))
+            sequences.append((id, r.query_sequence, r.reference_start))
 
     return sequences
 
@@ -37,13 +41,14 @@ def findMaxPosition(sequences):
 sequences = []
 
 k = 10
-bins = 20.0
+bins = 200.0
 swiftUrl = "http://130.238.29.253:8080/swift/v1/1000-genomes-dataset/"
 usage = "usage: python ./genome.py kmers|heatmap"
 
 sc = SparkContext("local", "Genome")
 files = sc.textFile("shortIndex.txt").cache()
 sequences = files.map(extractSequences).flatMap(lambda x: x)
+print(sequences.collect())
 
 if len(sys.argv) < 2:
 
@@ -54,14 +59,12 @@ elif sys.argv[1] == "kmers":
     allKMers = sequences.flatMap(lambda seq: applyKMers(seq, k)).groupByKey()
     manyKMers = allKMers.filter(lambda x : len(x[1]) > 1).map(lambda x: (len(x[1]), x[0])).sortByKey()
 
-    manyKMers.saveAsTextFile("sparkOutput")
+    manyKMers.saveAsTextFile("outputKmers")
 
 elif sys.argv[1] == "heatmap":
 
     maxPos = float(findMaxPosition(sequences))
-    result = sequences.map(lambda x: (round(x[1]/maxPos*float(bins)), 1)).reduceByKey(lambda a, b: (a, sum(b))).collect()
-    
-    print(result)
+    sequences.map(lambda x: (int(round((x[1]/maxPos*float(bins)))), 1)).reduceByKey(add).sortByKey().saveAsTextFile("outputHeatmap")
 
 else:
     print(usage)
